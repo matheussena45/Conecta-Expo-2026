acbe84ef75e6e5385429ec11598eb425f341e53d
Matheus Vinicius <155267013+matheussena45@users.noreply.github.com>
2026-03-27 16:42:43 -0300

    ajustes gerais e iniciação da tela adm

--- Projeto-Conecta 2026/script/script.js
+++ Projeto-Conecta 2026/script/script.js
@@ -17,13 +17,34 @@ const btnEntrar = document.getElementById("btnEntrar");
 btnEntrar.onclick = () => {
     const usuario = document.getElementById("usuario").value;
     const senha = document.getElementById("senha").value;
+    const erro = document.getElementById("erroLogin");
 
     const userCorreto = "admin";
-    const senhaCorreta = "#SebraeExpo30";
+    const senhaCorreta = "1234";
 
     if (usuario === userCorreto && senha === senhaCorreta) {
         window.location.href = "admin.html";
     } else {
-        alert("Usuário ou senha incorretos!");
+        erro.style.display = "block";
+        erro.textContent = "Usuário ou senha incorretos!";
     }
+};
+
+const inputs = document.querySelectorAll("#usuario, #senha");
+const erro = document.getElementById("erroLogin");
+
+inputs.forEach(input => {
+    input.addEventListener("input", () => {
+        erro.style.display = "none";
+    });
+});
+
+
+fechar.onclick = () => {
+    usuarioInput.value = "";
+    senhaInput.value = "";
+
+    erro.style.display = "none";
+
+    popup.style.display = "none";
 };
